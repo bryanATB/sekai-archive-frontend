36a534aa096dc54d600e209987a166fd698debb4
bryanATB <bryanbasado16@gmail.com>
2026-06-02 19:05:05 -0500

    update api url to production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://sekai-archive-backend.onrender.com/api/v1',
 })
 
 // Agrega el token automáticamente a cada petición
